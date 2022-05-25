f107354412704103cb5bcfa171181a42dd9e4df0
Mippoose <mippoose.m1l@gmail.com>
2022-05-26 01:14:28 +0600

    commands(lesson 2)

---
+++ RAGE/server-files/dotnet/resources/ServerSide/CommandAttributes/RequiresHealthAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using GTANetworkAPI;
+using GTANetworkMethods;
+using Player = GTANetworkAPI.Player;
+
+namespace ServerSide.CommandAttributes
+{
+    public class RequiresHealthAttribute : CommandConditionAttribute
+    {
+        public int Amount { get; set; }
+
+        public RequiresHealthAttribute(int amount = 100)
+        {
+            Amount = amount;
+        }
+        
+        public override bool Check(Player player, string cmdName, string cmdText)
+        {
+            if (player.Health > Amount)
+            {
+                player.SendChatMessage("Youre healthy af");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RAGE/server-files/dotnet/resources/ServerSide/Commands.cs
@@ -0,0 +1,46 @@
+﻿using GTANetworkAPI;
+using ServerSide.CommandAttributes;
+
+namespace ServerSide
+{
+    public class Commands : Script
+    {
+        [Command]
+        public void CreateCar(Player player)
+        {
+            NAPI.Vehicle.CreateVehicle(VehicleHash.Adder, player.Position, player.Heading, 131, 131);
+        }
+
+        [Command("hp")]
+        public void SetHealth(Player player, int count)
+        {
+            player.Health = count;
+        }
+
+        [Command("teleport", Alias = "tp")]
+        public void TeleportPlayer(Player player, float x, float y, float z)
+        {
+            player.Position = new Vector3(x, y, z);
+        }
+
+        [Command("armor")]
+        public void SetPlayerArmor(Player player, int armor = 100)
+        {
+            player.Armor = armor;
+        }
+
+        [Command("me", GreedyArg = true)]
+        public void TypeMe(Player player, string actions)
+        {
+            player.SendChatMessage($"{player.Name} did that: " + actions);
+        }
+
+        [RequiresHealth(75)]
+        [Command("healme")]
+        public void HealPlayer(Player player)
+        {
+            player.Health = 100;
+            player.SendChatMessage("Healed!");
+        }
+    }
+}
